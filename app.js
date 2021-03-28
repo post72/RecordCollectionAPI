4139b73d4c9dee8b30c02cad47fa42e4b7fe4e57
William Burch <post72@me.com>
2021-03-28 15:08:41 -0500

    Updating the app.js configuration and adding code comments

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8008,
-    mongoose = require('mongoose'),
-    Record = require('./api/models/recordModel'), //created model loading here
-    bodyParser = require('body-parser');
+require("dotenv").config(); // Require the dotenv
+const express = require('express'); // Require Express web framework for NodeJS
+const mongoose = require('mongoose'); // Require Mongoose - MongoDB schema /model validation library
+const Record = require('./api/models/recordModel'); // created model loading here
+const PORT = process.env.PORT || 8008; // create PORT from .env or use 8008
 
-var mongoConnectStr = 'mongodb://localhost/test';
+// create the MongoDB connection string from .env value or use mongodb://localhost/test (stand alone local database)
+const mongoConnectStr = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost/test';
 
 // mongoose instance connection url connection
+//************************************************ */
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoConnectStr, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose
+    .connect(mongoConnectStr, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false 
+    })
+    .then(() => {
+        console.log("Database connection Success!");
+    })
+    .catch((err) => {
+        console.error("Mongo Connection Error", err);
+    });
+//************************************************* */
+
+const app = express(); // Declare the Express Application for configuring, running and rendering the web framework
+
+// don't expose we use Express. need to know basis
+app.set('x-powered-by', false);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/commonApiRoutes'); //importing route
+const routes = require('./api/routes/commonApiRoutes'); //importing route
 routes(app); //register the route
 
 // App Error Middlewear
@@ -25,7 +42,7 @@ app.use(function (req, res) {
 });
 
 
-app.listen(port);
+app.listen(PORT);
 
 
-console.log('A music record catalogue RESTful API server started on: ' + port);
+console.log('A music record catalogue RESTful API server started on: ' + PORT);
