1d2a328a50b328181601933fc75e015d4555f36c
William Burch <bburch@bouncingpixel.com>
2022-02-09 14:11:44 -0600

    Successfully added jwtCheck to protected API routes

--- api/controllers/publicController.js
+++ api/controllers/publicController.js
@@ -6,18 +6,3 @@ exports.index = function (req, res) {
       message: "This is the Record List API!",
     });
 };
-
-
-exports.public = function (req, res) {
-    res.json({
-        type: 'public',
-        message: 'Hello from the public API!'
-    });
-};
-
-exports.private = function (req, res) {
-  res.json({
-    type: "private",
-    message: "This should be a SECURE API ENDPOINT!",
-  });
-};
--- api/routes/commonApiRoutes.js
+++ api/routes/commonApiRoutes.js
@@ -3,25 +3,20 @@
 module.exports = function (app) {
     var recordData = require('../controllers/recordController');
     var publicData = require('../controllers/publicController');
+    var jwtCheck = require('../utils/check-auth0-jwt');
 
     // OPEN Routes
     app.route('/')
         .get(publicData.index);
 
-    app.route('/public')
-        .get(publicData.public);
-
-    app.route("/private")
-        .get(publicData.private);
-
     // recordList Routes
-    app.route('/records')
-        .get(recordData.list_all_records)
-        .post(recordData.create_a_record);
+    app.route("/records")
+      .get(recordData.list_all_records)
+      .post(jwtCheck, recordData.create_a_record);
 
 
-    app.route('/records/:recordId')
-        .get(recordData.read_a_record)
-        .put(recordData.update_a_record)
-        .delete(recordData.delete_a_record);
+    app.route("/records/:recordId")
+      .get(recordData.read_a_record)
+      .put(jwtCheck, recordData.update_a_record)
+      .delete(jwtCheck, recordData.delete_a_record);
 };
